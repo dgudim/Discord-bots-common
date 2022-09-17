77a389b1e020023a98e3053ede5485223244b436
kloud-sauron <dgudim@gmail.com>
2022-09-17 16:32:50 +0300

    small fix

--- dist/utils/init_utils.d.ts
+++ dist/utils/init_utils.d.ts
@@ -1,4 +1,4 @@
 import { Client } from "discord.js";
 import { DKRCommands } from "dkrcommands";
-export declare function testEnvironmentVar(variable: string, var_name: string, exit: boolean): void;
+export declare function testEnvironmentVar(variable: string | undefined, var_name: string, exit: boolean): void;
 export declare function dkrInit(client: Client): DKRCommands;
--- src/utils/init_utils.ts
+++ src/utils/init_utils.ts
@@ -4,7 +4,7 @@ import path = require("path");
 import { colors, wrap } from "./colors";
 import { error, info, warn } from "./logger";
 
-export function testEnvironmentVar(variable: string, var_name: string, exit: boolean) {
+export function testEnvironmentVar(variable: string | undefined, var_name: string, exit: boolean) {
     if (!variable) {
         if (exit) {
             error(`${wrap(var_name, colors.LIGHTER_BLUE)} environment variable is not set, can't proceed`);
